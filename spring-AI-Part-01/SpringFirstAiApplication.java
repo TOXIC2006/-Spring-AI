4349c0ace1ba6e6c4c65fd4d3763c06739c4e314
Siddharth Singh Rawat <sidraw2006@gmail.com>
2026-09-22 00:42:34 +0530

    Add files via upload

---
+++ spring-AI-Part-01/SpringFirstAiApplication.java
@@ -0,0 +1,17 @@
+package com.lcdw.springai.springfirstai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(
+
+)
+public class SpringFirstAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFirstAiApplication.class, args);
+    }
+
+}
